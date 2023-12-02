fc96f4158cd721d3257760c5b32411a881908842
maskpa100 <maskpa@yandex.ru>
2023-12-02 20:51:26 +0800

    refactor

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,27 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
+const http = require('http').Server(app)
+const cors = require('cors')
+const socketIO = require('socket.io')(http,{
+  cors:{
+    origin:'http://localhost:5173'
+  }
+})
+
+socketIO.on('connection',(socket)=>{
+  console.log(`${socket.id} user connected`)
+  socket.on('disconnect',()=>{
+    console.log(`${socket.id} disconnect`)
+  })
+})
+
 app.get('/',(req, res)=>{
   res.json({
     message: 'start server'
   })
 })
-app.listen(PORT,()=>{
+
+http.listen(PORT,()=>{
   console.log('Server working')
 })
